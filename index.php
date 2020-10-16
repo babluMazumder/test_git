e8db9dbcfc07649caed3e5cd4f9af9dfe575a0cd
bablu_300 <bablupub@gmail.com>
2020-10-16 20:48:45 +0600

    remove master code

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
 
-// from master
-echo 'Hello! Im Master branch'.'<br>';
-
-// from master 2nd commit 
-echo 'Hello! Im Master branch'.'<br>';
-
 // 1st commit from fb 
 echo 'Hello! Im fb branch'.'<br>';
